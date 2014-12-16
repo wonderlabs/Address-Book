e93c33193e2e93e582e6407f68788f8e68445807
wonderlabs <vucenod@gmail.com>
2014-12-16 16:43:03 -0500

    Windows 8 Address Book Project

---
+++ Address Book/Address Book/Address Book.Windows/ContactDetailsPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Address_Book
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ContactDetailsPage : Page
+    {
+        public ContactDetailsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            var result = e.Parameter as Result;
+            if(result != null)
+            {
+                PopulateContactDetails(result);
+            }
+        }
+        private void PopulateContactDetails(Result result)
+        {
+            //Populate the field data
+            var usr = result.user;
+            var usrName = usr.name;
+
+            Title.Text = usrName.title;
+            First.Text = usrName.first;
+            Last.Text = usrName.last;
+
+            var usrLocation = usr.location;
+
+            Street.Text = usrLocation.street;
+            City.Text = usrLocation.city;
+            State.Text = usrLocation.state;
+            Zip.Text = usrLocation.zip;
+
+            //set profile image
+            ProfilePic.Source = new BitmapImage(new Uri(usr.picture.medium));
+
+            Email.Text = usr.email;
+            Username.Text = usr.username;
+            Password.Text = usr.password;
+
+            Gender.Text = usr.gender;
+            Dob.Text = usr.dob;
+            Hphone.Text = usr.phone;
+            Cphone.Text = usr.cell;
+        }
+
+        private void BackClicked(object sender, RoutedEventArgs e)
+        {
+            Frame frame = Window.Current.Content as Frame;
+            if (frame == null)
+            {
+                return;
+            }
+
+            if (frame.CanGoBack)
+            {
+                frame.GoBack();
+            }
+        }
+
+    }
+}
---
+++ Address Book/Address Book/Address Book.Windows/MainPage.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+//My added libs
+using System.Net;
+using System.Runtime.Serialization.Json;
+using Windows.Web.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+using Newtonsoft.Json;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Address_Book
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private HttpClient httpClient;
+        private HttpResponseMessage response;
+        private int numberOfResults = 25; //Set the number of contacts to load.
+        RootObject root = new RootObject();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            httpClient = new HttpClient();
+
+            // Add a user-agent header
+            var headers = httpClient.DefaultRequestHeaders;
+            headers.UserAgent.ParseAdd("ie");
+            headers.UserAgent.ParseAdd("Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)");
+
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            LoadContacts();
+        }
+
+        private async void LoadContacts()
+        {
+            response = new HttpResponseMessage();
+
+            string jsonResult;
+            string numResults = numberOfResults.ToString(); //set the number of results you want
+            string randomUserUrl = "http://api.randomuser.me/?results={0}";
+            string randomuserUrlLink = string.Format(randomUserUrl, numberOfResults);
+
+            Uri url = new Uri(randomuserUrlLink, UriKind.Absolute);
+
+
+            try
+            {
+                response = await httpClient.GetAsync(url);
+
+                response.EnsureSuccessStatusCode();
+
+                jsonResult = await response.Content.ReadAsStringAsync();
+
+            }
+            catch (Exception ex)
+            {
+
+                jsonResult = "";
+            }
+
+            PopulateList(jsonResult);
+
+        }
+
+        private void PopulateList(string json)
+        {
+            root = JsonConvert.DeserializeObject<RootObject>(json);
+
+            List<Result> contactsList = root.results;
+            contacts.DataContext = contactsList;
+        }
+
+
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Add code to perform some action here.
+
+            ListBox selection = sender as ListBox;
+            if (selection != null)
+            {
+                var result = selection.SelectedItem;
+                var selectedResult = result as Result;
+                if(selectedResult != null)
+                {
+                    //push new page displaying contact information, pass in Result
+                    this.Frame.Navigate(typeof(ContactDetailsPage), selectedResult);
+                }
+                
+            }
+
+        }
+    }
+}
